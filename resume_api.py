2aff586329447cc8d4612179f27a2ea788ee86fe
Justin Katzwhite <jkatzwhite@gmail.com>
2019-02-18 17:08:13 -0500

    Added comments to api

--- resume_api.py
+++ resume_api.py
@@ -6,21 +6,25 @@ from data import Programming_Languages
 
 package = 'Resume'
 
+# property class for messages
 class Properties(messages.Message):
     number = messages.IntegerField(1, variant=messages.Variant.INT32)
     text = messages.StringField(2)
     image_key = messages.StringField(3)
 
+# Data classs for data to be sent
 class Data(messages.Message):
     data = messages.MessageField(Properties, 1, repeated=True)
 
 PL = Data()
 
+# Backend API
 @endpoints.api(name='resume', version='v1')
 class ResumeApi(remote.Service):
 
   QUERY_ID = endpoints.ResourceContainer(text = messages.StringField(1))
-
+  
+# method takes programming language selected and returns corresponding information about it
   @endpoints.method(QUERY_ID, Data,
                     path='pl/{text}', http_method='GET',
                     name='list.pL')
